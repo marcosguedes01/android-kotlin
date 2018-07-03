e8f7c2067a71a0475caaed78c2310e6cae7d25c9
Marcos Guedes <marcosguedes01@gmail.com>
2018-07-03 11:23:08 -0300

    Layout netflix add

---
+++ app/src/main/java/com/programacaobrasil/aprendendokotlin/NetflixActivity.kt
@@ -0,0 +1,12 @@
+package com.programacaobrasil.aprendendokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NetflixActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_netflix)
+    }
+}
